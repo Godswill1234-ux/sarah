4727ed0554daea550d5ddf5f8a6708cd023b6b36
Godswill1234-ux <godswillumek2023@gmail.com>
2024-09-17 12:30:56 +0100

    first commit
    java

---
+++ Java/First.java
@@ -0,0 +1,45 @@
+
+import java.lang.classfile.instruction.IncrementInstruction;
+
+ 
+
+//PUBLIC CLASS
+public class First {
+
+    public static void main(String[] args) { //MAIN METHOD
+        System.out.println("Welcome To Java Class");
+
+ //type  holder  value
+        boolean b = true;
+        boolean wrong = false;
+
+
+        //name = "Mike";
+        //PRIMITIVE- NUMERIC  - INTERGER 
+        int age = 23;
+        long money = 23456783;
+
+
+        //FLOATING TYPES
+        double numb = 589483.9; //64 bits
+        float num =234; //32 bits
+        char sex ='M';
+
+        // REFERENCE TYPES- STRING, ARRAY, ENUMS
+        String name = "Mike";
+        String time = "The bus is yellow";
+        System.out.println("My name is" + name);
+        
+
+        int a=20;
+        int d=6;
+        int c=a%d;
+        c--;
+        System.out.println(c);
+        
+      
+
+
+    }
+
+}
---
+++ Java/Second.java
@@ -0,0 +1,11 @@
+public class Second{
+public static void main(String[] args) { //Main method
+    System.out.println("This is my second attempt");
+
+
+
+    //type holder
+    
+}
+    
+}
